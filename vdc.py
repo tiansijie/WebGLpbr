f64a91f48a7524ffce56c17421043f4417a0dfa9
Sijie Tian <sijie@socialtables.com>
2015-04-21 00:21:18 -0400

    New sampleing

---
+++ vdc.py
@@ -0,0 +1,10 @@
+def vdc(n, base=2):
+    vdc, denom = 0,1
+    while n:
+        denom *= base
+        n, remainder = divmod(n, base)
+        vdc += remainder / denom
+    return vdc
+
+
+[vdc(i) for i in range(10)]
